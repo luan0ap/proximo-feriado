22e577474ac7fd84846dd6d48c6834590c7685a0
Alex Figueiredo <alexxfsilva@gmail.com>
2017-03-24 00:44:04 -0300

    Initial version

--- index.js
+++ index.js
@@ -3,17 +3,15 @@
 'use strict';
 
 const fs       = require('fs');
-const moment   = require('moment');
 const holidays = JSON.parse(fs.readFileSync('holidays.json', 'utf8'));
+const moment   = require('moment');
 const now      = moment.now();
 
 moment.locale('pt-br');
 
-let nextHolidays = holidays
-    .filter(holiday => moment(holiday.timestamp).isAfter(now))
-    .map(holiday => moment(holiday.timestamp));
+let next  = holidays.filter(holiday => moment(holiday.timestamp).isAfter(now))[0];
+next.time = moment(next.timestamp);
 
-let next = moment.min(nextHolidays);
-
-console.log(next.format("dddd, DD [de] MMMM."));
-console.log('Faltam ' + next.fromNow());
+console.log(next.name);
+console.log(next.time.format("dddd, DD [de] MMMM."));
+console.log('Faltam ' + next.time.fromNow(true));
